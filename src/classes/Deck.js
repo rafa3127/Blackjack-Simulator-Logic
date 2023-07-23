71cd6d41b0ebc88db5a63fa339cc30b56936d1e6
rafael <rafael@3dlinkweb.com>
2023-07-23 14:25:39 -0400

    added logic for deck class

---
+++ src/classes/Deck.js
@@ -0,0 +1,52 @@
+import { SUITS, RANKS } from "../constants.js";
+import { Card } from "./Card.js";
+
+export class Deck {
+  #deck;
+  #numberOfDecks;
+
+  constructor(numberOfDecks = 1) {
+    this.#numberOfDecks = numberOfDecks;
+    this.#deck = this.generateDeck();
+  }
+
+  generateDeck() {
+    let deck = [];
+    for (let i = 0; i < this.#numberOfDecks; i++) {
+      for (let suit of SUITS) {
+        for (let rank of RANKS) {
+          deck.push(new Card(rank.name, suit.name));
+        }
+      }
+    }
+    return deck;
+  }
+
+  shuffleDeck() {
+    for (let i = this.#deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.#deck[i], this.#deck[j]] = [this.#deck[j], this.#deck[i]];
+    }
+  }
+
+  sortDeck() {
+    this.#deck.sort((a, b) => {
+      if (SUITS.indexOf(a.getCardDetails().suit) - SUITS.indexOf(b.getCardDetails().suit) === 0) {
+        return RANKS.indexOf(a.getCardDetails().rank) - RANKS.indexOf(b.getCardDetails().rank);
+      }
+      return SUITS.indexOf(a.getCardDetails().suit) - SUITS.indexOf(b.getCardDetails().suit);
+    });
+  }
+
+  drawCard() {
+    return this.#deck.pop();
+  }
+
+  count() {
+    return this.#deck.length;
+  }
+
+  isEmpty() {
+    return this.#deck.length === 0;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
 import { Card } from './classes/Card.js';
+import { Deck } from './classes/Deck.js';
 import { RANKS, SUITS } from './constants.js';
 
-//Use nested forEach to generate a deck
-SUITS.forEach((suit) => {
-  RANKS.forEach((rank) => {
-    const card = new Card(rank.name, suit.name);
-    console.log(card.toString());
-  });
-});
+const main = () => {
+    // testCardClass()
+    testDeck()
+
+}
+
+
+const testCardClass = () => {
+    //Use nested forEach to generate a deck
+    SUITS.forEach((suit) => {
+      RANKS.forEach((rank) => {
+        const card = new Card(rank.name, suit.name);
+        console.log(card.toString());
+      });
+    });
+}
+
+const testDeck = () => {
+    const deck = new Deck(6);
+    deck.shuffleDeck()
+
+    console.log(`Count: ${deck.count()}`);
+    console.log('x'.repeat(50));
+
+    for(let i = 0; i < 10; i++) {
+        const card = deck.drawCard();
+        console.log(card.toString());
+    }
+
+    console.log(`Count: ${deck.count()}`);
+    console.log('x'.repeat(50));
+
+    while(!deck.isEmpty()) {
+        const card = deck.drawCard();
+        console.log(card.toString());
+    }
+
+    console.log(`Count: ${deck.count()}`);
+}
+
+
+
+
+
+main()
